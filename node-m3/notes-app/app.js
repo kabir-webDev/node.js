a87ae994b4596e6f3f4be4f2a49986fb9464d10a
kabir-webDev <kabir.webdev@gmail.com>
2021-04-24 23:09:53 +0600

    Request Fetch

--- node-m3/notes-app/app.js
+++ node-m3/notes-app/app.js
@@ -1,56 +1,8 @@
-const chalk = require('chalk')
-const yargs = require('yargs')
-const notes = require('./notes.js')
+const request = require("request");
 
-// Customize yargs version
-yargs.version('1.1.0')
+const url =
+  "http://api.weatherstack.com/current?access_key=8bef2d752753a8ff0527696ef477d8ef&query=Dhaka";
 
-// Create add command
-yargs.command({
-    command: 'add',
-    describe: 'Add a new note',
-    builder: {
-        title: {
-            describe: 'Note title',
-            demandOption: true,
-            type: 'string'
-        },
-        body: {
-            describe: 'Note body',
-            demandOption: true,
-            type: 'string'
-        }
-    },
-    handler: function (argv) {
-        notes.addNote(argv.title, argv.body)
-    }
-})
-
-// Create remove command
-yargs.command({
-    command: 'remove',
-    describe: 'Remove a note',
-    handler: function () {
-        console.log('Removing the note')
-    }
-})
-
-// Create list command
-yargs.command({
-    command: 'list',
-    describe: 'List your notes',
-    handler: function () {
-        console.log('Listing out all notes')
-    }
-})
-
-// Create read command
-yargs.command({
-    command: 'read',
-    describe: 'Read a note',
-    handler: function () {
-        console.log('Reading a note')
-    }
-})
-
-yargs.parse()
+request({ url: url, json: true }, (error, res) => {
+  console.log(res.body.current);
+});
