facf62417d93fd3c41aece9bd0d374371cbcd902
kabir-webDev <kabir.webdev@gmail.com>
2021-05-09 05:23:56 +0600

    Sub App

--- LWS-MERN/app.js
+++ LWS-MERN/app.js
@@ -1,16 +1,15 @@
 const express = require("express");
-
 const app = express();
+const adminRoute = express.Router();
 
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("This is home page");
+adminRoute.get("/dashboard", (req, res) => {
+  res.send("This is admin Dashboard!");
 });
 
-app.post("/", (req, res) => {
-  console.log(req.body.name);
-  res.send("This is post Man! ");
+app.use("/admin", adminRoute);
+
+app.get("/user/:id", (req, res) => {
+  res.send("Hello World");
 });
 
 app.listen(1111, () => {
