3e894e3c7c851b9d105a99abb7aa00e3124e18cb
kabir-webDev <kabir.webdev@gmail.com>
2021-05-09 07:48:31 +0600

    dash

--- LWS-MERN/app.js
+++ LWS-MERN/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const adminRoute = express.Router();
 
+app.use(express.json());
+
 adminRoute.get("/dashboard", (req, res) => {
   res.send("This is admin Dashboard!");
 });
@@ -9,7 +11,13 @@ adminRoute.get("/dashboard", (req, res) => {
 app.use("/admin", adminRoute);
 
 app.get("/user/:id", (req, res) => {
+  console.log(req.params.id);
   res.send("Hello World");
+});
+
+app.post("/user", (req, res) => {
+  console.log(req.body.name);
+  res.send("Hello World Post!");
 });
 
 app.listen(1111, () => {
