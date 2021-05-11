1b361fe68e5484db8171acba1e471a8d23f75fea
kabir-webDev <kabir.webdev@gmail.com>
2021-05-11 06:19:16 +0600

    CRUD operation with Mongoose

--- LWS-MERN/app.js
+++ LWS-MERN/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const goHandler = require("./gohan/goHandler");
 
 app.use(express.json());
 
@@ -20,6 +21,8 @@ function errorHandler(err, req, res, next) {
   }
   res.status(500).json({ error: err });
 }
+
+app.use("/todo", goHandler);
 
 app.listen(1111, () => {
   console.log("Server is running Bro");
