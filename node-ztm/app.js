bf8d2c445eaed66425e7e155c6a1c3b9ba193d6e
Kabir Hasan <kabir.webdev@gmail.com>
2021-06-01 23:00:19 +0600

    nodem

--- node-ztm/app.js
+++ node-ztm/app.js
@@ -1,1 +1,11 @@
-console.log("Boom");
+const express = require("express");
+
+const app = express();
+const PORT = 1111;
+app.get("/", (req, res) => {
+  res.send("This is coolest");
+});
+
+app.listen(PORT, () => {
+  console.log("App is up and running");
+});
