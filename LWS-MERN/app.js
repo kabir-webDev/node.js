29a0cd106659b3d0c54384a7950c26ebf1c4d5f0
Kabir Hasan <kabir.webdev@gmail.com>
2021-06-28 20:44:00 +0600

    Stated again bros

--- LWS-MERN/app.js
+++ LWS-MERN/app.js
@@ -1,29 +1,12 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const app = express();
-const goHandler = require("./gohan/goHandler");
 
-app.use(express.json());
+const PORT = 1111;
 
-mongoose
-  .connect("mongodb://localhost/todos", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Database is connected");
-  })
-  .catch((err) => console.log(err));
+app.get("/", (req, res) => {
+  res.send("I'm Back Code World ... ☯️");
+});
 
-function errorHandler(err, req, res, next) {
-  if (res.headersSent) {
-    return next(err);
-  }
-  res.status(500).json({ error: err });
-}
-
-app.use("/todo", goHandler);
-
-app.listen(1111, () => {
-  console.log("Server is running Bro..");
+app.listen(PORT, () => {
+  console.log("I'm Backing you up Bro 😄");
 });
