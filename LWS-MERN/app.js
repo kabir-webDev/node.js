c92123751241b73ef6c48e094bd4c69395a6a4fd
Kabir Hasan <kabir.webdev@gmail.com>
2021-07-04 01:15:26 +0600

    MongoDB-Connection

--- LWS-MERN/app.js
+++ LWS-MERN/app.js
@@ -1,8 +1,19 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 const PORT = 1111;
 
+//initialization of app
+const app = express();
 app.use(express.json());
+
+//database connection with mongoose
+mongoose
+  .connect("mongodb://localhost/todoApp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connection Successful"))
+  .catch((err) => console.log(err));
 
 function errorHandler(err, req, res, next) {
   if (res.headerSent) {
