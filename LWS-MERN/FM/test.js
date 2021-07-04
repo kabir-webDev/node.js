63436f659ccc94ebdda56a78394b339adef29892
Kabir Hasan <kabir.webdev@gmail.com>
2021-07-05 01:06:47 +0600

    MongoDB Methods

--- LWS-MERN/FM/test.js
+++ LWS-MERN/FM/test.js
@@ -1,21 +1,39 @@
 const mongoose = require("mongoose");
 
 const connect = () => {
-  return mongoose.connect("mongodb://localhost:27017/student", {
+  return mongoose.connect("mongodb://localhost:27017/ribak", {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   });
 };
 
-const student = mongoose.Schema({
-  name: String,
+const user = mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  favFoods: [{ type: String }],
+  conNumber: {
+    type: Number,
+    required: true,
+  },
 });
 
-const Student = mongoose.model("student", student);
+const User = mongoose.model("user", user);
 
 connect()
   .then(async (connection) => {
-    const stu = await Student.create({ name: "Kabir" });
-    console.log(stu);
+    // const user = await User.create({
+    //   firstName: "Kabirul Hasan",
+    //   lastName: "Sikder",
+    //   favFoods: ["Khicuri", "Biriani"],
+    //   conNumber: 01763180249,
+    // });
+    const findUser = await User.findById("60e1f9e101854f0ccc6e4b02");
+    console.log(findUser);
   })
-  .catch((e) => console.log(e));
+  .catch((error) => console.log(error));
