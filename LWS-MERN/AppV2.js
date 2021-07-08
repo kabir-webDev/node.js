ac15e50b1ded7fd8d80b2c1d4ba94da6b635662d
Kabir Hasan <kabir.webdev@gmail.com>
2021-07-08 19:46:08 +0600

    Find and Update

--- LWS-MERN/AppV2.js
+++ LWS-MERN/AppV2.js
@@ -1,8 +1,37 @@
 const mongoose = require("mongoose");
 
 const shelf1 = new mongoose.Schema({
+  id: mongoose.Types.ObjectId,
   name: String,
   company: String,
   price: Number,
   useFor: String,
 });
+
+const RAK = mongoose.model("medicine", shelf1);
+
+const run = async () => {
+  await mongoose
+    .connect("mongodb://localhost/medicineApp", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("Connection Successful"))
+    .catch((err) => console.log(err));
+  // const create = await RAK.create({
+  //   name: "Cinkara",
+  //   company: "Hamdard",
+  //   price: 120,
+  //   useFor: "Vitamin",
+  // });
+  const product = "60e7000c6fdd4822e8e3a1aa";
+  const findOut = await RAK.findByIdAndUpdate(
+    product,
+    {
+      name: "Sinkara",
+    },
+    { new: true }
+  ).exec();
+  console.log(findOut);
+};
+run();
